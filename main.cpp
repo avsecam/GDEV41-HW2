0f253571d08c65d96142a473d2b8a360299b9d0e
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-08-30 17:01:57 +0800

    Add Particle struct with constructor.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <raylib.h>
 
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+
+struct Particle {
+  bool isActive;
+  Vector2 position;
+  Vector2 direction;
+  float speed;
+  float lifeTime;
+  Color color;
+
+  Particle() {
+    isActive = false;
+    position.x = 0.0f;
+    position.y = 0.0f;
+    direction.x = 0.0f;
+    direction.y = 0.0f;
+    speed = 0.0f;
+    lifeTime = 0.0f;
+    color.r = 255;
+    color.g = 0;
+    color.b = 0;
+    color.a = 255;
+  }
+};
+
+
 int main() {
-  InitWindow(800, 600, "Hello Raylib");
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game Loop, Input Handling, and Frames");
   while (!WindowShouldClose()) {
       BeginDrawing();
       EndDrawing();
